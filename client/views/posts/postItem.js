e0b9f56d8b3c64c8f9a56b1c459358fe697d02a4
iboxgithub <ibox@laposte.net>
2014-09-07 18:44:25 +0200

    going further

--- client/views/posts/postItem.js
+++ client/views/posts/postItem.js
@@ -1,6 +1,10 @@
 /**
  * Created by ibox on 8/30/14.
  */
+//animations
+var POST_HEIGHT = 80;
+var Positions = new Meteor.Collection(null);//null = local
+
 Template.postItem.helpers({
     ownPost: function() {
         return this.userId == Meteor.userId(); //use of session id
@@ -21,6 +25,24 @@ Template.postItem.helpers({
         } else {
             return 'disabled';
         }
+    },
+    attributes: function() {
+        //TODO relire fin chap Animations
+        var post = _.extend({}, Positions.findOne({postId: this._id}), this);
+        var newPosition = post._rank * POST_HEIGHT;
+        var attributes = {};
+        if (! _.isUndefined(post.position)) {
+           // attributes.class = "post invisible";
+        //} else { TODO not working cf chapt Animations
+            var delta = post.position - newPosition;
+            attributes.style = "top: " + delta + "px";
+            if (delta === 0)
+                attributes.class = "post animate"
+        }
+        Meteor.setTimeout(function() {//position is defined here
+            Positions.upsert({postId: post._id}, {$set: {position: newPosition}})
+        });
+        return attributes;
     }
 });
 Template.postItem.events({
--- client/views/posts/postsList.js
+++ client/views/posts/postsList.js
@@ -5,3 +5,15 @@
 });
 removed because handled in router (pagination)
 */
+
+//pour animations
+Template.postsList.helpers({
+    postsWithRank: function() {
+        this.posts.rewind();
+        return this.posts.map(function(post, index, cursor) {
+            //ajout de l'attribut rank aux objets postsWithRank dans PostsList
+            post._rank = index;
+            return post;
+        });
+    }
+});
